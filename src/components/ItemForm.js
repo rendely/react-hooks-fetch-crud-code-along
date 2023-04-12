179d1616aa6c5f121b67ed6f026cb323bc0d7872
matthew rendely <matthew.rendely@gmail.com>
2023-04-11 18:21:45 -0700

    show items, post item

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,8 +1,32 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({onAddItem}) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
+  const itemsAPI = 'http://localhost:4000/items';
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log('fetching');
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false
+    }
+
+    fetch(itemsAPI, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(itemData)
+    })
+      .then(r => r.json())
+      .then((newItem) => onAddItem(newItem));
+
+      
+  }
 
   return (
     <form className="NewItem">
@@ -29,7 +53,7 @@ function ItemForm() {
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" onClick={handleSubmit}>Add to List</button>
     </form>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,7 +6,23 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+  const itemsAPI = 'http://localhost:4000/items';
 
+  useEffect(() => {
+    fetch(itemsAPI, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+      .then(r => r.json())
+      .then(items => setItems(items))
+  },[]
+  )
+
+  function handleAddItem(newItem){
+     setItems([...items, newItem]);
+  }
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
@@ -19,7 +35,7 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
